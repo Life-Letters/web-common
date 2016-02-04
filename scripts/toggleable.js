8915ebba7d965234fc1dbaac89d5539df6dcf8f7
Torben Sko <torben.sko@gmail.com>
2016-02-05 00:23:29 +1100

    Added a toggle directive, to help with the navbar (used externally)

---
+++ scripts/toggleable.js
@@ -0,0 +1,46 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name themeApp.directive:toggleable
+ * @description
+ * # toggleable
+ */
+angular.module('life.theme', [])
+  .directive('toggleable', function () {
+    return {
+  		scope: true,
+      restrict: 'A',
+      link: function(scope, element, attrs) {
+        scope.on = false || attrs.toggleable;
+      },
+      controller: function($scope) {
+        $scope.toggle = function() {
+          $scope.on = !$scope.on;
+        };
+        $scope.turnOn = function() {
+          $scope.on = true;
+        };
+        $scope.turnOff = function() {
+          $scope.on = false;
+        };
+        $scope.isOn = function() {
+          return $scope.on;
+        };
+        $scope.isOff = function() {
+          return !$scope.on;
+        };
+      }
+    };
+  })
+  .directive('glyphicon', function () {
+    return {
+      scope: true,
+      restrict: 'E',
+      replace: true,
+      template: 
+        '<i class="glyphicon" ng-class="{'+
+          '\'glyphicon-chevron-down\': isOn(),'+
+          '\'glyphicon-chevron-right\': isOff()}"></i>',
+    };
+  });
