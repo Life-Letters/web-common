0b382421e848725bb861179bd4e29298cc2c9e1f
Patrick Lai <patrick.lai@lifeletters.com.au>
2016-03-23 12:49:17 +1100

    Added Centering bootstrap modal more Added Consults icon in site nav

--- scripts/directives/site-nav.js
+++ scripts/directives/site-nav.js
@@ -1,81 +1,85 @@
 'use strict';
 
 angular.module('life.common')
-  .directive('siteNav', function ($log, $sce, users) {
+  .directive('siteNav', function($log, $sce, users) {
     return {
       template: '<span ng-include="path"></span>',
       restrict: 'E',
       scope: true,
       link: function(scope, element, attrs) {
-        if ( !window.urls || !window.urls.ehr || !window.urls.website ) {
+        if (!window.urls || !window.urls.ehr || !window.urls.website) {
           $log.error('missing window.urls');
           return;
         }
         scope.path = $sce.trustAsResourceUrl(
-            (window.urls && window.urls.websiteAppViews ? window.urls.websiteAppViews : '')+'views/web-common/site-nav.html');
+          (window.urls && window.urls.websiteAppViews ? window.urls.websiteAppViews : '') + 'views/web-common/site-nav.html');
 
-        scope.navItemGrps = 
-          [[
-            {
-              name: 'tests',
-              label: 'Tests',
-              icon: 'test',
-              path: window.urls.website+'tests',
-            },{
-              name: 'articles',
-              label: 'Articles',
-              icon: 'news',
-              path: window.urls.website+'articles',
-            },{
-              name: 'referrals',
-              label: 'Referral',
-              icon: '123',
-              path: window.urls.website+'referrals',
-            },{
-              name: 'help',
-              label: 'Help',
-              icon: 'question',
-              path: window.urls.website+'help',
-            }
-          ],[
-            {
-              name: 'login',
-              label: 'Login',
-              icon: 'user',
-              path: window.urls.ehr+'login',
-              user: false,
-            },{
-              name: 'users',
-              label: 'Me',
-              icon: 'user',
-              path: window.urls.ehr+'records',
-              user: 'Patient',
-            },{
-              name: 'patients',
-              label: 'Patients',
-              icon: 'report',
-              path: window.urls.ehr+'patients',
-              user: 'Clinician',
-            },{
-              name: 'actions',
-              label: 'Actions',
-              icon: 'report',
-              path: window.urls.ehr+'actions',
-              user: 'Clinician',
-            },{
-              name: 'refer',
-              label: 'Refer',
-              icon: '123',
-              path: window.urls.ehr+'refer',
-              user: 'Clinician',
-            },{
-              name: 'logout',
-              label: 'Logout',
-              icon: 'question',
-              path: window.urls.ehr+'logout',
-              user: true,
-            }
-          ]];
+        scope.navItemGrps = [
+          [{
+            name: 'tests',
+            label: 'Tests',
+            icon: 'test',
+            path: window.urls.website + 'tests',
+          }, {
+            name: 'articles',
+            label: 'Articles',
+            icon: 'news',
+            path: window.urls.website + 'articles',
+          }, {
+            name: 'referrals',
+            label: 'Referral',
+            icon: '123',
+            path: window.urls.website + 'referrals',
+          }, {
+            name: 'help',
+            label: 'Help',
+            icon: 'question',
+            path: window.urls.website + 'help',
+          }],
+          [{
+            name: 'login',
+            label: 'Login',
+            icon: 'user',
+            path: window.urls.ehr + 'login',
+            user: false,
+          }, {
+            name: 'users',
+            label: 'Me',
+            icon: 'user',
+            path: window.urls.ehr + 'records',
+            user: 'Patient',
+          }, {
+            name: 'patients',
+            label: 'Patients',
+            icon: 'report',
+            path: window.urls.ehr + 'patients',
+            user: 'Clinician',
+          }, {
+            name: 'consults',
+            label: 'Consults',
+            icon: 'report',
+            path: window.urls.ehr + 'consults',
+            user: 'Clinician',
+          }, {
+            name: 'actions',
+            label: 'Actions',
+            icon: 'report',
+            path: window.urls.ehr + 'actions',
+            user: 'Clinician',
+          }, {
+            name: 'refer',
+            label: 'Refer',
+            icon: '123',
+            path: window.urls.ehr + 'refer',
+            user: 'Clinician',
+          }, {
+            name: 'logout',
+            label: 'Logout',
+            icon: 'question',
+            path: window.urls.ehr + 'logout',
+            user: true,
+          }]
+        ];
       },
     };
   });
