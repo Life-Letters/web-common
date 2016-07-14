a56096d67e79f090d03f1ec8e39d960972e55753
Patrick Lai <patrick.lai@lifeletters.com.au>
2016-07-14 11:51:56 +1000

    Lazy Load will retry until success

--- scripts/directives/lazy-load.js
+++ scripts/directives/lazy-load.js
@@ -9,6 +9,12 @@ angular.module('life.common')
     return {
     	link: function postLink(scope, element, attrs) {
 				var img = new Image();
+        var retryTimeout = 5000;
+        var interval = null;
+        var spinner = angular.element('<div class="loading"></div>');
+
+        // Put a spinner
+        element.after(spinner);
 
 				img.onload = function () {
 					if ( attrs.lazyLoad ) {
@@ -21,10 +27,28 @@ angular.module('life.common')
 								break;
 						}
 					}
+
 					element.addClass('lazy_loaded');
+          // Clear the looping interval to retry
+          clearInterval(interval);
+          // Remove Spinner
+          element.next().remove();
 				};
+
 				img.src = attrs.lazyLoad || attrs.src || element.css('background-image').replace(/^url\("|"\)$/g, '');
-				// console.log(img.src);
+
+        // Try again after some seconds
+        img.onerror = function(){
+          // Only do this once
+          if(!interval){
+            interval = setInterval(function (){
+                // Try again
+                img.src += '?' + +new Date;
+                element.attr('src', img.src);
+            }, retryTimeout);
+          }
+        };
+
 			}
   	};
   });
